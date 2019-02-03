2fee6df0a1fe6f2567cfa13cb7ca71cc5a597fd1
Zac <zaclew922@gmail.com>
2019-02-04 09:03:10 +1100

    Router and page setup

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,26 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Home";
+import Contact from "./Components/Contact";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <Route path="/" exact component={Home} />
+          <Route path="/contact" exact component={Contact} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ portfolio/src/Components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Zac Lew - Portfolio</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
