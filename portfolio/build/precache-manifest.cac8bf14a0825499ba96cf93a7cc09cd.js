b55d94ce40002d05eb96237bece01ed111088955
Zac <zaclew922@gmail.com>
2019-02-04 16:15:11 +1100

    Server setup

---
+++ portfolio/build/precache-manifest.cac8bf14a0825499ba96cf93a7cc09cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3dce67091c7ae8e754cd05b7ced7b64",
+    "url": "/static/media/logo.f3dce670.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "353c21f8a6ec07b0cf40",
+    "url": "/static/js/main.353c21f8.chunk.js"
+  },
+  {
+    "revision": "49e63993645ff29f0104",
+    "url": "/static/js/1.49e63993.chunk.js"
+  },
+  {
+    "revision": "353c21f8a6ec07b0cf40",
+    "url": "/static/css/main.ba9fe4b5.chunk.css"
+  },
+  {
+    "revision": "49e63993645ff29f0104",
+    "url": "/static/css/1.6a27eb5b.chunk.css"
+  },
+  {
+    "revision": "aa085dbda71cbeddb966f480b2158a41",
+    "url": "/index.html"
+  }
+];
