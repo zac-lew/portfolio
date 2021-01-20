6f7ae9f5fa7ce6ad40499052ffab2dd9ba49cf15
zaclew922 <zaclew922@gmail.com>
2021-01-20 15:03:59 +1100

    Added social icons

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,6 +5,18 @@ const Footer = () => {
   return (
     <div className="footer">
       <h1>Get in contact!</h1>
+      <a href="https://www.facebook.com/zacl922/" target="_blank" rel="noopener noreferrer">
+        <i class="icon fab fa-facebook"></i>
+      </a>
+      <a href="https://www.instagram.com/zac_lew/" target="_blank" rel="noopener noreferrer">
+        <i class="icon fab fa-instagram"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/zac-lew-1a616614a/" target="_blank" rel="noopener noreferrer">
+        <i class="icon fab fa-linkedin"></i>
+      </a>
+      <a href="mailto:zaclew922@gmail.com">
+        <i class="icon fas fa-envelope-open-text"></i>
+      </a>
       <div className="copyright">Copyright &copy; 2021 Zac Lew</div>
     </div>
   );
