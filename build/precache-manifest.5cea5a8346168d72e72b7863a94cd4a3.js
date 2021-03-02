c020e6dacb7454d9172aa54f0014416f5504c9ca
zaclew922 <zaclew922@gmail.com>
2021-03-03 10:15:50 +1100

    Added manual redirect and updated build files

---
+++ build/precache-manifest.5cea5a8346168d72e72b7863a94cd4a3.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66cfb398b93c7e036bfd2fb1d9a347f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "347a276d69a5cd529188",
+    "url": "/static/css/2.7ca58fb4.chunk.css"
+  },
+  {
+    "revision": "7063c6be4ac219604191",
+    "url": "/static/css/main.210cedba.chunk.css"
+  },
+  {
+    "revision": "347a276d69a5cd529188",
+    "url": "/static/js/2.85990862.chunk.js"
+  },
+  {
+    "revision": "7063c6be4ac219604191",
+    "url": "/static/js/main.aa4cce4d.chunk.js"
+  },
+  {
+    "revision": "015eb96dc6d20e45ac85",
+    "url": "/static/js/runtime~main.8d83e2bd.js"
+  },
+  {
+    "revision": "4195cf989fac0128a89669f40a1e3496",
+    "url": "/static/media/bootstrap.4195cf98.png"
+  },
+  {
+    "revision": "de2cf3ea9b841229f08851463b340d2e",
+    "url": "/static/media/cplusplus.de2cf3ea.png"
+  },
+  {
+    "revision": "3c28c740a3210a6197d712e1cf492788",
+    "url": "/static/media/css3.3c28c740.png"
+  },
+  {
+    "revision": "4a1bd78803e54dbf2a2247273d2ddcb4",
+    "url": "/static/media/dashboard.4a1bd788.png"
+  },
+  {
+    "revision": "3495e5b378fee9aaa0bb2df6ae11b51e",
+    "url": "/static/media/git.3495e5b3.png"
+  },
+  {
+    "revision": "2cb94ab70a7485fc74bdba20f7802de1",
+    "url": "/static/media/heroku.2cb94ab7.png"
+  },
+  {
+    "revision": "6947fe3a6ee4b2ca9b1cdceb231ddc32",
+    "url": "/static/media/jasmine.6947fe3a.png"
+  },
+  {
+    "revision": "c647884d2940f119b665a453f9c2c8f3",
+    "url": "/static/media/js.c647884d.png"
+  },
+  {
+    "revision": "f3dce67091c7ae8e754cd05b7ced7b64",
+    "url": "/static/media/logo.f3dce670.png"
+  },
+  {
+    "revision": "0fab12847e7771ade81144c5feda884a",
+    "url": "/static/media/matlab.0fab1284.png"
+  },
+  {
+    "revision": "b48dfd5259f57d69fe350fd73bc71ba0",
+    "url": "/static/media/mobileprofile.b48dfd52.png"
+  },
+  {
+    "revision": "a331387718d805c9720d3730d4e895c2",
+    "url": "/static/media/mysql.a3313877.png"
+  },
+  {
+    "revision": "5830a1f92ddbf5a129f1fdd8285fcc0c",
+    "url": "/static/media/node.5830a1f9.png"
+  },
+  {
+    "revision": "88ec78cfe4c2cb212081eb3287c185c4",
+    "url": "/static/media/php.88ec78cf.png"
+  },
+  {
+    "revision": "42d4b5d80652d29f8b657307fc0d793a",
+    "url": "/static/media/powershell.42d4b5d8.png"
+  },
+  {
+    "revision": "21df5bbb05ef8b16fe6e661c07870743",
+    "url": "/static/media/profile.21df5bbb.png"
+  },
+  {
+    "revision": "fa28e8bfbb406ab6bdd9e96f33f66335",
+    "url": "/static/media/ps.fa28e8bf.png"
+  },
+  {
+    "revision": "5c6e848b7e49156f810fd5e47677e3d6",
+    "url": "/static/media/qlikview.5c6e848b.png"
+  },
+  {
+    "revision": "37fe8322b169ddbdeabf75930e886ac6",
+    "url": "/static/media/react.37fe8322.png"
+  },
+  {
+    "revision": "37fe8322b169ddbdeabf75930e886ac6",
+    "url": "/static/media/reactnative.37fe8322.png"
+  },
+  {
+    "revision": "8251fe76d7487a28b07603515b300964",
+    "url": "/static/media/redux.8251fe76.png"
+  },
+  {
+    "revision": "6597cc35589236a430e20f4d2adf09a6",
+    "url": "/static/media/sass.6597cc35.png"
+  },
+  {
+    "revision": "3cff2040d9381621faffcd9de67d19db",
+    "url": "/static/media/thesis.3cff2040.png"
+  },
+  {
+    "revision": "57df402aced8152248a914b8b20e6e96",
+    "url": "/static/media/westpac.57df402a.png"
+  }
+]);
--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 //server.js
-const sslRedirect = require("heroku-ssl-redirect").default;
 const express = require("express");
 const favicon = require("express-favicon");
 const path = require("path");
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 const app = express();
+
+app.use(function (req, res, next) {
+  if (req.secure) {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // request was via http, so redirect to https
+    res.redirect("https://" + req.headers.host + req.url);
+  }
+});
+
 app.use(favicon(__dirname + "/build/favicon.ico"));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
-app.use(sslRedirect());
 app.get("/ping", function (req, res) {
   return res.send("pong");
 });
