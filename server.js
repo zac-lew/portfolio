776f11645cb21fef3ca08aaf312988fb08af9880
zaclew922 <zaclew922@gmail.com>
2021-03-03 09:23:48 +1100

    Added SSL redirect to force HTTPS

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 //server.js
+import sslRedirect from "heroku-ssl-redirect";
 const express = require("express");
 const favicon = require("express-favicon");
 const path = require("path");
@@ -8,10 +9,11 @@ app.use(favicon(__dirname + "/build/favicon.ico"));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
-app.get("/ping", function(req, res) {
+app.use(sslRedirect());
+app.get("/ping", function (req, res) {
   return res.send("pong");
 });
-app.get("/*", function(req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 app.listen(port);
