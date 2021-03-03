8fbd30ae3ecf78420569848e3548e8f087ac9816
zaclew922 <zaclew922@gmail.com>
2021-03-03 13:29:30 +1100

    Moved redirect code to index.html

--- server.js
+++ server.js
@@ -2,19 +2,8 @@
 const express = require("express");
 const favicon = require("express-favicon");
 const path = require("path");
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8080;
 const app = express();
-
-app.use(function (req, res, next) {
-  if (req.secure) {
-    // request was via https, so do no special handling
-    next();
-  } else {
-    // request was via http, so redirect to https
-    res.redirect("https://" + req.headers.host + req.url);
-  }
-});
-
 app.use(favicon(__dirname + "/build/favicon.ico"));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
