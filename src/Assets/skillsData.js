db6ca891754ff0bb1d1f2bddc08c6197fd380b9c
Zac Lew <zaclew922@gmail.com>
2025-01-13 16:33:59 +0100

    Update skills list

--- src/Assets/skillsData.js
+++ src/Assets/skillsData.js
@@ -1,23 +1,17 @@
 export default [
   {
     skillsTitle: "Front-End",
-    skillsDescription:
-      "Front-end and UI dev is where I began my web and software journey. From teaching myself basic JS, to deploying React code to enterpise production enviroments.",
-    skillsList: ["HTML5", "CSS3", "JavaScript", "React", "Redux", "Angular8"],
+    skillsList: ["HTML", "CSS", "JavaScript", "React", "Jest"],
     skillsIcon: "fa-code",
   },
   {
     skillsTitle: "Back-End",
-    skillsDescription:
-      "Back-end and API development is where I am currently spending most of my time, and where I am learning the most. This is my focus as I transition from a front-end to a full-stack developer.",
-    skillsList: ["Java", "Springboot", "SQL", "Node", "Express.js"],
+    skillsList: ["Node.js", "Java (Springboot)", "Ruby (Rails)"],
     skillsIcon: "fa-database",
   },
   {
     skillsTitle: "Other Tools",
-    skillsDescription:
-      "Through my study of Mechatronic Engineering and my many varied roles at Westpac I have developed skills with a plethora of useful technologies, including data management and visualisation tools.",
-    skillsList: ["C", "C++", "Powershell", "Microsoft PowerBI", "QlikView", "Matlab"],
+    skillsList: ["PostgreSQL", "MySQL", "Docker", "AWS", "CI/CD pipelines"],
     skillsIcon: "fa-chart-bar",
   },
 ];
